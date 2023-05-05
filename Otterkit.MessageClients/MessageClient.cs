cf4f74945397df8ecb9ac8c6fb285730fb7ccb49
Keith Taylor <KTSnowy@outlook.com>
2023-05-05 19:44:31 -0300

    [Refactor]: Remove unnecessary old message client

--- Otterkit.MessageClients/MessageClient.cs
+++ Otterkit.MessageClients/MessageClient.cs
@@ -5,49 +5,5 @@ namespace Otterkit.MessageClients;
 
 public class MessageClient
 {
-    static string OtterSock = $"{Path.GetTempPath()}otter.sock";
 
-    readonly HttpClient internalClient;
-
-    public MessageClient(string? baseAddress = null)
-    {
-        var socketHandler  = new SocketsHttpHandler
-        {
-            ConnectCallback = static async (context, token) =>
-            {
-                var socket = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.IP);
-
-                var endpoint = new UnixDomainSocketEndPoint(OtterSock);
-
-                await socket.ConnectAsync(endpoint).SetupContext();
-
-                return new NetworkStream(socket, false);
-            }
-        };
-
-        internalClient = new HttpClient(socketHandler, true);
-
-        if (baseAddress is not null)
-        {
-            internalClient.BaseAddress = new Uri(baseAddress);
-        }
-    }
-
-    public async ValueTask<Memory<byte>> ReceiveAsync(string endpoint)
-    {
-        var response = await internalClient.GetAsync(endpoint).SetupContext();
-        
-        return await response.Content.ReadAsByteArrayAsync().SetupContext();
-    }
-
-    public async ValueTask<Memory<byte>> SendAsync(string endpoint, MessageTag tag)
-    {
-        var request = new HttpRequestMessage(HttpMethod.Post, endpoint);
-
-        request.Content = new ByteArrayContent(tag.Message);
-
-        var post = await internalClient.SendAsync(request);
-
-        return await post.Content.ReadAsByteArrayAsync().SetupContext();
-    }
 }
